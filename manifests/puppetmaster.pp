adb4f9e82d691a56609ec464ce8249e29a7b7452
Matthew Kennedy <mkennedy@clearwateranalytics.com>
2013-05-10 15:46:07 -0600

    Name module correctly

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,4 +1,4 @@
-class puppet_common::puppetmaster ( $git_ssh_key ) {
+class puppet::puppetmaster ( $git_ssh_key ) {
   package { 'puppetmaster-passenger':
     ensure  => installed,
     require => Apt::Source[ 'puppetlabs' ],
