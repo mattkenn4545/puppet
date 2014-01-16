af26d69341fc3e452a1c4ce46fac1321d8ce4d46
Matthew Kennedy <mkennedy@clearwateranalytics.com>
2014-01-16 14:33:20 -0700

    Cleanup

--- manifests/master.pp
+++ manifests/master.pp
@@ -7,60 +7,60 @@ class puppet::master (
   else {  $pin_ensure = 'present' }
 
   apt::pin { 'puppetmaster-passenger':
-    ensure   => $pin_ensure,
-    packages => 'puppetmaster-passenger',
-    version  => $version,
-    priority => 1001,
+    ensure    => $pin_ensure,
+    packages  => 'puppetmaster-passenger',
+    version   => $version,
+    priority  => 1001,
   }
 
   apt::pin { 'puppetmaster-common':
-    ensure   => $pin_ensure,
-    packages => 'puppetmaster-common',
-    version  => $version,
-    priority => 1001,
+    ensure    => $pin_ensure,
+    packages  => 'puppetmaster-common',
+    version   => $version,
+    priority  => 1001,
   }
 
   package { 'puppetmaster-passenger':
-    ensure  => latest,
-    before  => Service[ 'puppet' ],
+    ensure    => latest,
+    before    => Service[ 'puppet' ],
   }
 
   package { 'rubygems':
-    ensure => present,
+    ensure    => present,
   }
 
   host { 'puppet':
-    ensure  => present,
-    ip      => '127.0.1.1',
-    before  => Service[ 'puppet' ],
+    ensure    => present,
+    ip        => '127.0.1.1',
+    before    => Service[ 'puppet' ],
   }
 
   file { '/etc/puppet/auth.conf':
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    source  => "puppet:///modules/${module_name}/auth.conf",
-    require => Package[ 'puppetmaster-passenger' ],
+    mode      => '0644',
+    owner     => 'root',
+    group     => 'root',
+    source    => "puppet:///modules/${module_name}/auth.conf",
+    require   => Package[ 'puppetmaster-passenger' ],
   }
 
   file { '/etc/puppet/environments':
-    ensure => directory,
-    mode    => '0755',
-    owner   => $environment_dir_owner,
-    group   => $environment_dir_owner,
-    require => Package[ 'puppetmaster-passenger' ],
+    ensure    => directory,
+    mode      => '0755',
+    owner     => $environment_dir_owner,
+    group     => $environment_dir_owner,
+    require   => Package[ 'puppetmaster-passenger' ],
   }
 
   cron { 'reports cleanup':
-    command => 'find /var/lib/puppet/reports/* -mtime +7 -type f -exec rm -rf {} \;',
-    user    => root,
-    hour    => 3,
-    minute  => 30
+    command   => 'find /var/lib/puppet/reports/* -mtime +7 -type f -exec rm -rf {} \;',
+    user      => root,
+    hour      => 3,
+    minute    => 30
   }
 
   file { '/etc/puppet/autosign.conf':
-    ensure  => present,
-    content  => $autosign,
-    require => File['/etc/puppet/environments'],
+    ensure    => present,
+    content   => $autosign,
+    require   => File['/etc/puppet/environments'],
   }
 }
