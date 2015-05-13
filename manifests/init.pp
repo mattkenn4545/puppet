cd4528e1cbe19e6fb54e5e03f2a945f9e5ca5bb3
Matthew Kennedy <mkennedy@clearwateranalytics.com>
2015-05-13 17:30:17 -0600

    Use full fingerprint

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@ class puppet (
   apt::source { 'puppetlabs':
     location   => 'http://apt.puppetlabs.com',
     repos      => 'main dependencies',
-    key        => '1054B7A24BD6EC30',
+    key        => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
     key_server => 'pgp.mit.edu',
   }
 
