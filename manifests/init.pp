1c4d298ef43c8e3f073a007e0b5e747bc1ff641d
tgeorge <tgeorge@clearwateranalytics.com>
2016-06-28 18:29:32 -0600

    Update for new version of apt

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class puppet (
     repos      => 'main dependencies',
     key        => {
       'id'     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-      'server' => 'pgp.mit.edu'
+      'server' => 'pgp.mit.edu',
     },
   }
 
